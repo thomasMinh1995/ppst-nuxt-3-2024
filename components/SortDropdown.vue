3efe18e34304e576c78a5d0fcb093738e6038ddb
nguyenducminh10 <nguyenducminh051595@gmail.com>
2024-08-20 17:55:17 +0700

    Aug-20-2024: add new sortDropdown component and update loadmore api with sort api

---
+++ components/SortDropdown.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <label for="sort-select">Sort By:</label>
+    <select id="sort-select" v-model="selectedSort" @change="emitSort">
+      <option value="title:asc">Title (Ascending)</option>
+      <option value="title:desc">Title (Descending)</option>
+      <option value="date:asc">Date (Ascending)</option>
+      <option value="date:desc">Date (Descending)</option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+    const emit = defineEmits(['update:sort']);
+
+    const selectedSort = ref('title:asc');
+
+    const emitSort = () => {
+    emit('update:sort', selectedSort.value);
+    };
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="grid grid-cols-4 gap-4">
+    <SortDropdown @update:sort="handleSortChange" />
     <div v-if="loading">Loading....</div>
     <NuxtLink
       v-else
@@ -27,18 +28,20 @@ definePageMeta({
 const posts = ref([]);
 const limit = 10;
 const currentPage = ref(1);
-const loading = ref(true);
+const loading = ref(false);
+const sortCriteria = ref("title:asc"); // Default sort criteria
 
 //Fetch data posts
 const fetchPosts = async () => {
-  const pending = status === "pending";
-  loading.value = pending;
+  const [field, order] = sortCriteria.value.split(":");
   const url = `https://dummyjson.com/posts?limit=${limit}&skip=${
     (currentPage.value - 1) * limit
-  }`;
-  const { data, error, status } = await useFetch(url, {
+  }&sortBy=${field}&order=${order}`;
+  const { data, error } = await useFetch(url, {
     server: false,
   });
+
+  //check if API response is array or object.
   if (data.value && Array.isArray(data.value.posts)) {
     if (data.value.posts.length < limit) {
       loading.value = false;
@@ -59,9 +62,18 @@ const loadMore = () => {
   currentPage.value += 1;
   fetchPosts();
 };
-
-// Initial fetch when the component mounts
-onMounted(() => {
+// Handle sort change
+const handleSortChange = (newSort) => {
+  sortCriteria.value = newSort;
+  currentPage.value = 1; // Reset to first page
+  posts.value = []; // Clear current posts
   fetchPosts();
+};
+
+// Watch for changes in sort criteria and refetch posts
+watch(sortCriteria, (newSort) => {
+  handleSortChange(newSort);
 });
+// Initial fetch when the component mounts
+fetchPosts();
 </script>

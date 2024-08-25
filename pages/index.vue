ecf62289e7c743a0a9ed800f32ebe5aa1f322ddf
nguyenducminh10 <nguyenducminh051595@gmail.com>
2024-08-25 22:54:35 +0700

    Aug-25-2024: add fetch get quote

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,31 @@
 <template>
     <div>
-        <h1>Hello</h1>
+        <h1>This is an home page</h1>
     </div>
 </template>
 
-<script>
-    export default {
-        
-    }
+<script setup>
+// const { data: quotes, error } = await useFetch('https://dummyjson.com/quotes', {
+//     server: false
+// });
+
+// console.log('data._object', quotes.value.quotes)
+// console.log('data._object', quotes._value)
+
+
+// const data = await useFetch('https://dummyjson.com/quotes', {
+//     server: false
+// });
+
+// const item = data.data.value;
+// console.log('hello', item)
+
+// export default {
+//     async setup() {
+        const { data, error } = await useFetch('https://dummyjson.com/quotes');
+
+        console.log('data', data.value?.quotes)
+        // return { quotes: data.value?.quotes, error: error.value };
+//     }
+// }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
